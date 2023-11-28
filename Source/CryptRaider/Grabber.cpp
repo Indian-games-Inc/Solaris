8f69f2824604f1358c9c7a0e348845111b07c4fd
Plaksa Illia <iluhaplaksa@gmail.com>
2023-11-28 03:04:23 +0200

    Init commit

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,100 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle && PhysicsHandle->GetGrabbedComponent())
+	{
+		FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+		PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+	}
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
+{
+	return GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+}
+
+bool UGrabber::GetGrabbableInReach(FHitResult& HitResult) const
+{
+	FVector Start = GetComponentLocation() + GetForwardVector();
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Cyan);
+
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
+
+	bool HasHit = GetWorld()->SweepSingleByChannel(
+		HitResult, Start, End,
+		FQuat::Identity,
+		ECC_GameTraceChannel2,
+		Sphere
+	);
+
+	return HasHit;
+}
+
+void UGrabber::Grab()
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	FHitResult HitResult;
+	if (PhysicsHandle && GetGrabbableInReach(HitResult))
+	{
+		UPrimitiveComponent* HitComponent = HitResult.GetComponent();
+		HitComponent->WakeAllRigidBodies();
+		HitComponent->SetSimulatePhysics(true);
+
+		auto* Actor = HitResult.GetActor();
+		Actor->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+		Actor->Tags.Add(GrabbedTag);
+		
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			HitComponent,
+			NAME_None,
+			HitResult.ImpactPoint,
+			GetComponentRotation()
+		);
+	}
+}
+
+void UGrabber::Release()
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (!PhysicsHandle) { return; }
+
+	if (auto* GrabbedComponent = PhysicsHandle->GetGrabbedComponent(); GrabbedComponent)
+	{
+		AActor* Actor = GrabbedComponent->GetOwner();
+		Actor->Tags.Remove(GrabbedTag);
+		
+		PhysicsHandle->ReleaseComponent();
+	}
+}
+
+
