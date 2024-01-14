a50db7e7aa9c5399977150cd1017efbf2419ca89
Plaksa Illia <iluhaplaksa@gmail.com>
2024-01-14 22:27:37 +0200

    Update CryptRaiderGameMode.cpp

--- Source/CryptRaider/CryptRaiderGameMode.cpp
+++ Source/CryptRaider/CryptRaiderGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "CryptRaiderGameMode.h"
-#include "CryptRaiderCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ACryptRaiderGameMode::ACryptRaiderGameMode()
