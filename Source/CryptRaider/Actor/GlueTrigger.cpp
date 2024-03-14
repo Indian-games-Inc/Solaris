2ba69186dc9fba8cd9c47bf146e0c14ebc1b3d52
Grechyshkin Danil <danil.grechishkin@gmail.com>
2024-03-14 23:43:51 +0100

    [glue-items] Reworked structure

---
+++ Source/CryptRaider/Actor/GlueTrigger.cpp
@@ -0,0 +1,36 @@
+﻿#include "GlueTrigger.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AGlueTrigger::AGlueTrigger()
+{
+	Root = CreateDefaultSubobject<USceneComponent>(FName("Root"));	
+	SetRootComponent(Root);
+	
+	//Trigger box
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(FName("Trigger Box"));
+	TriggerBox->SetupAttachment(RootComponent);
+
+	//Ladder box
+	LadderBox = CreateDefaultSubobject<UBoxComponent>(FName("Ladder Box"));
+	LadderBox->SetupAttachment(RootComponent);
+
+	//Target
+	// const ConstructorHelpers::FObjectFinder<UStaticMesh> MeshObj(TEXT("/Content/Map/_GENERATED/DarkMinimum/Box_F5DA6FED.uasset"));
+	Target = CreateDefaultSubobject<UStaticMeshComponent>(FName("Target"));
+	Target->SetupAttachment(RootComponent);
+	// Target->SetStaticMesh(MeshObj.Object);
+}
+
+void AGlueTrigger::OnConstruction(const FTransform& Transform)
+{
+	
+	// TriggerBox->SetBoxExtent(TriggerArea, true);
+	// LadderBox->SetBoxExtent(LadderArea, true);
+	LadderBox->SetVisibility(IsDebug, true);
+	TriggerBox->SetVisibility(IsDebug, true);
+
+	Super::OnConstruction(Transform);
+}
---
+++ Source/CryptRaider/Actor/GlueTrigger.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GlueTrigger.generated.h"
+
+
+class UBoxComponent;
+
+UCLASS()
+class CRYPTRAIDER_API AGlueTrigger : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGlueTrigger();
+	//
+	// protected:
+	// 	// Called when the game starts or when spawned
+	// 	virtual void BeginPlay() override;
+
+	void OnConstruction(const FTransform& Transform) override;
+
+
+private:
+	USceneComponent* Root = nullptr;
+
+	//List of the exposed values in UE
+	UPROPERTY(EditInstanceOnly)
+	class AItem* ItemClass;
+	// UPROPERTY(EditInstanceOnly)
+	// FVector TriggerArea;
+	// UPROPERTY(EditInstanceOnly)
+	// FTransform TargetTransform;
+	// UPROPERTY(EditInstanceOnly)
+	// FVector LadderArea;
+	// UPROPERTY(EditInstanceOnly)
+	// FTransform LadderTransform;
+	UPROPERTY(EditInstanceOnly)
+	bool IsClimbable;
+	UPROPERTY(EditInstanceOnly)
+	bool IsDebug;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* Target;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* TriggerBox = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* LadderBox = nullptr;
+};
