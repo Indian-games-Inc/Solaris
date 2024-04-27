baca77c6915bf0b89196268ed65dc2c17b098ade
Plaksa Illia <iluhaplaksa@gmail.com>
2024-04-27 16:21:35 +0300

    Minor headers refactor

--- Source/CryptRaider/Component/Inventory.cpp
+++ Source/CryptRaider/Component/Inventory.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Inventory.h"
-#include "../Data/InventoryItemWrapper.h"
+#include "CryptRaider/Data/InventoryItemWrapper.h"
 
 void UInventory::InitItemsArray()
 {
--- Source/CryptRaider/Component/Picker.cpp
+++ Source/CryptRaider/Component/Picker.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Picker.h"
-#include "../Actor/Item.h"
-#include "../Data/InventoryItemWrapper.h"
+#include "CryptRaider/Actor/Item.h"
+#include "CryptRaider/Data/InventoryItemWrapper.h"
 
 
 TOptional<FInventoryItemWrapper> UPicker::PickItem(const FHitResult& HitResult)
